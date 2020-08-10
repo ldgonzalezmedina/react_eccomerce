aa7e5ff070fc8d0193da21bab594a0d2e809d6e6
unknown <gonzalez.luis@correo.unimet.edu.ve>
2020-08-10 12:52:03 -0500

    Eccomerce_Project_Setup

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Directory } from "../../Components/directory/directory.component";
+import './homepage.style.scss'
+export const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+);
