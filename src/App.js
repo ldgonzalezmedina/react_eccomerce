45df209dd27b3f31aaa0f327ecce9e08757ab3db
ldgonzalezmedina <gonzalez.luis@correo.unimet.edu.ve>
2020-09-03 12:06:33 -0500

    sign up component and method to save users added

--- src/App.js
+++ src/App.js
@@ -8,20 +8,35 @@ import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 import Header from "./Components/header/header.component";
 import SignInSignUp from "./pages/SignInSignUp/SignInSignUp.component.jsx"
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      CurrentUser: null
+      currentUser: null
     }
   }
   unsubscribe = null;
 
   componentDidMount(){
-    this.unsubscribe=auth.onAuthStateChanged(user => {
-      this.setState({CurrentUser:user}, ()=>console.log(this.state.CurrentUser));
+    this.unsubscribe=auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      }else {
+        this.setState({currentUser:null}, ()=>console.log(this.state.CurrentUser));
+      }
+      
     })
   }
   componentWillUnmount(){
@@ -30,7 +45,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={this.state.CurrentUser}/>
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={Shop}/>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -22,4 +22,25 @@ const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const createUserProfileDocument = async(userAuth, addiotionalData) => {
+  if(!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  if(!snapshot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...addiotionalData
+      })
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+}
 export default firebase;
