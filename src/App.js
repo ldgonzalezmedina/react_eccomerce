7c08f51f3903bdc45eb5d0ecea9a2fc39206d2f5
ldgonzalezmedina <gonzalez.luis@correo.unimet.edu.ve>
2020-10-14 15:35:58 -0500

    added checkout page, checkout item and cart dropdown updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 
 import { Switch, Route, Redirect} from "react-router-dom";
 import { connect } from "react-redux";
+
 import HomePage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
-import Header from "./Components/header/header.component";
 import SignInSignUp from "./pages/SignInSignUp/SignInSignUp.component.jsx"
+import Checkout from "./pages/checkout/checkout.component";
+
+import Header from "./Components/header/header.component";
 import { auth, createUserProfileDocument} from "./firebase/firebase.utils";
+
 import { setCurrentUser } from "./redux/user/user.actions"
+import {selectCurrentUser} from "./redux/user/user.selectors"
+import {createStructuredSelector} from 'reselect'
+
 
 class App extends React.Component {
   unsubscribe = null;
@@ -41,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={Shop}/>
+          <Route exact path='/checkout' component={Checkout}/>
           <Route exact path='/signIn' render={()=> this.props.currentUser ? (<Redirect to ='/'/>) : (<SignInSignUp/>)}/>
         </Switch>
       </div>
@@ -48,8 +56,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) =>({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 //dispatch --> a way for redux to know whatever u pass me its going to be an action object that im going to pass to every reducer
